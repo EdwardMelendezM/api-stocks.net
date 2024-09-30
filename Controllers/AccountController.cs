cafd54dc177e57008f31749dca125307483f28e7
edwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-09-30 11:38:09 -0500

    add changes

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Dtos.Account;
+using api.Interface;
 using api.Models;
+using api.Service;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -14,13 +17,22 @@ namespace api.Controllers
     public class AccountController: ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public AccountController(UserManager<AppUser> userManager)
+        public AccountController(
+            UserManager<AppUser> userManager,
+            ITokenService tokenService,
+            SignInManager<AppUser> signInManager
+            )
         {
             _userManager = userManager;
+            _tokenService = tokenService;
+            _signInManager = signInManager;
         }
 
         [HttpPost]
+        [Route("Register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
             try
@@ -43,11 +55,48 @@ namespace api.Controllers
                 {
                     return StatusCode(400, roleResult);
                 }
-                return Ok(result);
+                return Ok(new UserDto
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Token = _tokenService.CreateToken(user)
+                });
             }
             catch (System.Exception)
             {
-                return StatusCode(500 , "Internal server error");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            try
+            {
+                var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
+                if (user == null)
+                {
+                    return StatusCode(404, "User not found");
+                }
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+                if (!result.Succeeded)
+                {
+                    return StatusCode(401, "Invalid password");
+                }
+
+                return Ok(new UserDto
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Token = _tokenService.CreateToken(user)
+                });
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(500, "Internal Server Error");
             }
         }
     }
